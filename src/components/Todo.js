a6ba371ac0e9bad0171e19138b6227f35ac2b2c3
Thanh Dat <thanhdatwg@gmail.com>
2021-04-13 15:58:59 +0000

    add tabs

--- src/components/Todo.js
+++ src/components/Todo.js
@@ -11,13 +11,10 @@ import React, { useState } from 'react';
 import TodoItem from './TodoItem';
 import Input from './Input';
 import Filter from './Filter';
-
 /* カスタムフック */
 import useStorage from '../hooks/storage';
-
 /* ライブラリ */
 import {getKey} from "../lib/util";
-
 function Todo() {
   const [items, putItems] = React.useState([
       /* テストコード 開始 */
@@ -26,25 +23,47 @@ function Todo() {
     { key: getKey(), text: '明日の準備をする', done: false },
     /* テストコード 終了 */
   ]);
-    const handleAdd = text => {
+
+  const [filter, setFilter] = React.useState('ALL');
+
+  const displayItems = items.filter(item => {
+    if (filter === 'ALL') return true;
+    if (filter === 'TODO') return !item.done;
+    if (filter === 'DONE') return item.done;
+  });
+
+  const handleCheck = checked => {
+    const newItems = items.map(item => {
+      if (item.key === checked.key) {
+        item.done = !item.done;
+      }
+      return item;
+    });
+    putItems(newItems);
+  };
+  const handleAdd = text => {
     putItems([...items, { key: getKey(), text, done: false }]);
   };
- 
+
+  const handleFilterChange = value => setFilter(value);
 
   return (
     <div className="panel">
       <div className="panel-heading">
         ITSS ToDoアプリ
       </div>
-       <Input onAdd={handleAdd} />
-      {items.map(item => (
-       <TodoItem item={item} />
+      <Input onAdd={handleAdd} />
+      <Filter
+        onChange={handleFilterChange}
+        value={filter}
+      />
+      {displayItems.map(item => (
+        <TodoItem key={item.key} item={item} onCheck={handleCheck} />
       ))}
       <div className="panel-block">
-        {items.length} items
+        {displayItems.length} items
       </div>
     </div>
   );
 }
-
 export default Todo;
